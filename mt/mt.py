63ea45cc7984d8e2f855e8c7546f7ff17a904b0d
York Ng <yorkhayng@gmail.com>
2025-05-30 00:03:16 +0900

    code for simplified evaluation process

--- mt/mt.py
+++ mt/mt.py
@@ -9,88 +9,87 @@ import matplotlib.pyplot as plt
 import letor_metrics
 
 
-data = pd.read_csv('mt.csv')
-# updates = pd.read_csv('mt_updated.csv')
-# features = [ 'GENETIC','SYNTACTIC','FEATURAL','PHONOLOGICAL','INVENTORY','GEOGRAPHIC']
-# for feature in features:
-#     data[feature] = updates[feature]
-groups = data['Source lang']
-logo = LeaveOneGroupOut()
-# Experiments with ALL, DATASET, and URIEL, respectively
-features = [
-    'Overlap word-level', 'Overlap subword-level', 'Transfer lang dataset size',
-    'Target lang dataset size', 'Transfer over target size ratio', 'Transfer lang TTR',
-    'Target lang TTR', 'Transfer target TTR distance', 'GENETIC','SYNTACTIC','FEATURAL','PHONOLOGICAL','INVENTORY','GEOGRAPHIC'
-]
-# features = ['Overlap word-level', 'Overlap subword-level', 'Transfer lang dataset size',
-#             'Target lang dataset size', 'Transfer over target size ratio', 'Transfer lang TTR',
-#             'Target lang TTR', 'Transfer target TTR distance']
-# features = ['GENETIC', 'SYNTACTIC', 'FEATURAL', 'PHONOLOGICAL', 'INVENTORY', 'GEOGRAPHIC']
+def mt(filename, features):
+    data = pd.read_csv(filename)
+    # updates = pd.read_csv('mt_selected.csv')
+    # features = [ 'GENETIC','SYNTACTIC','FEATURAL','PHONOLOGICAL','INVENTORY','GEOGRAPHIC']
+    # for feature in features:
+    #     data[feature] = updates[feature]
+    groups = data['Source lang']
+    logo = LeaveOneGroupOut()
+    # Experiments with ALL, DATASET, and URIEL, respectively
+    # features = [
+    #     'Overlap word-level', 'Overlap subword-level', 'Transfer lang dataset size',
+    #     'Target lang dataset size', 'Transfer over target size ratio', 'Transfer lang TTR',
+    #     'Target lang TTR', 'Transfer target TTR distance', 'GENETIC','SYNTACTIC','FEATURAL','PHONOLOGICAL','INVENTORY','GEOGRAPHIC'
+    # ]
+    # features = ['Overlap word-level', 'Overlap subword-level', 'Transfer lang dataset size',
+    #             'Target lang dataset size', 'Transfer over target size ratio', 'Transfer lang TTR',
+    #             'Target lang TTR', 'Transfer target TTR distance']
+    # features = ['GENETIC', 'SYNTACTIC', 'FEATURAL', 'PHONOLOGICAL', 'INVENTORY', 'GEOGRAPHIC']
 
-data['relevance'] = 0
+    data['relevance'] = 0
 
-# assign relevances from 10 to 0
-for source_lang in data['Source lang'].unique():
-    source_lang_data = data[data['Source lang'] == source_lang].copy()
-    source_lang_data['rank'] = source_lang_data['BLEU'].rank(method='min', ascending=False)
-    top_indices = source_lang_data[source_lang_data['rank'] <= 10].index
-    data.loc[top_indices, 'relevance'] = 11 - source_lang_data.loc[top_indices, 'rank']
+    # assign relevances from 10 to 0
+    for source_lang in data['Source lang'].unique():
+        source_lang_data = data[data['Source lang'] == source_lang].copy()
+        source_lang_data['rank'] = source_lang_data['BLEU'].rank(method='min', ascending=False)
+        top_indices = source_lang_data[source_lang_data['rank'] <= 10].index
+        data.loc[top_indices, 'relevance'] = 11 - source_lang_data.loc[top_indices, 'rank']
 
-groups = data['Source lang']
-ndcg_scores = []
+    groups = data['Source lang']
+    ndcg_scores = []
 
 
-def func(i):
-    ranker = LGBMRanker(
-        boosting_type='gbdt',
-        objective='lambdarank',
-        n_estimators=100,
-        metric='lambdarank',
-        num_leaves=16,
-        min_data_in_leaf=5,
-        random_state=i,
-        feature_fraction=0.8,
-        verbose=-1
-    )
-    #leave one language out
-    for train_idx, test_idx in logo.split(data, groups=groups):
-        train_data = data.iloc[train_idx]
-        test_data = data.iloc[test_idx]
+    def func(i):
+        ranker = LGBMRanker(
+            boosting_type='gbdt',
+            objective='lambdarank',
+            n_estimators=100,
+            metric='lambdarank',
+            num_leaves=16,
+            min_data_in_leaf=5,
+            random_state=i,
+            feature_fraction=0.8,
+            verbose=-1
+        )
+        #leave one language out
+        for train_idx, test_idx in logo.split(data, groups=groups):
+            train_data = data.iloc[train_idx]
+            test_data = data.iloc[test_idx]
 
-        train_X = train_data[features]
-        train_y = train_data['relevance']
-        test_X = test_data[features]
-        test_y = test_data['relevance']
+            train_X = train_data[features]
+            train_y = train_data['relevance']
+            test_X = test_data[features]
+            test_y = test_data['relevance']
 
-        train_group_sizes = train_data.groupby('Transfer lang').size().tolist()
+            train_group_sizes = train_data.groupby('Transfer lang').size().tolist()
 
-        train_dataset = lgb.Dataset(train_X, label=train_y,group=train_group_sizes)
-        test_dataset = lgb.Dataset(test_X, label=test_y, group=[len(test_y)], reference=train_dataset)
+            train_dataset = lgb.Dataset(train_X, label=train_y,group=train_group_sizes)
+            test_dataset = lgb.Dataset(test_X, label=test_y, group=[len(test_y)], reference=train_dataset)
 
-        # Train
-        ranker.fit(train_X, train_y, group=train_group_sizes, verbose=-1)
+            # Train
+            ranker.fit(train_X, train_y, group=train_group_sizes)
 
-        # Predict and evaluate NDCG@3
-        y_pred = ranker.predict(test_X)
-        #linear ndcg
-        ndcg = ndcg_score([test_y.values], [y_pred], k=3)
-        #exp ndcg
-        # ndcg = letor_metrics.ndcg_score(test_y.values,y_pred,3)
-        ndcg_scores.append(ndcg)
+            # Predict and evaluate NDCG@3
+            y_pred = ranker.predict(test_X)
+            #linear ndcg
+            ndcg = ndcg_score([test_y.values], [y_pred], k=3)
+            #exp ndcg
+            # ndcg = letor_metrics.ndcg_score(test_y.values,y_pred,3)
+            ndcg_scores.append(ndcg)
 
-    # Calculate the average NDCG@3 score
-    print([round(float(x),3) for x in ndcg_scores])
-    average_ndcg = np.mean(ndcg_scores)
-    print(f'Average NDCG@3: {average_ndcg * 100}')
+        # Calculate the average NDCG@3 score
+        print([round(float(x),3) for x in ndcg_scores])
+        average_ndcg = np.mean(ndcg_scores)
+        print(f'Average NDCG@3: {average_ndcg * 100}')
+        return average_ndcg * 100
 
-    #final model)
-    # ranker.fit(data[features], data['relevance'], group=[53] * 54)
-    # print(ranker.feature_importances_)
-    # lightgbm.plot_importance(ranker, importance_type='split')
-    # plt.show()
-    # ranker.booster_.save_model('LightGBM_model.txt')
-a = []
-for i in range(10,30):
-    a.append(func(i))
-print(a)
+        #final model)
+        # ranker.fit(data[features], data['relevance'], group=[53] * 54)
+        # print(ranker.feature_importances_)
+        # lightgbm.plot_importance(ranker, importance_type='split')
+        # plt.show()
+        # ranker.booster_.save_model('LightGBM_model.txt')
+    return func(0)
 
---
+++ replace_distances.py
@@ -0,0 +1,71 @@
+from urielplus import urielplus as uriel
+import pandas as pd
+
+
+def replace(filename):
+    u = uriel.URIELPlus()
+    dep_df = pd.read_csv('dep/dep_updated.csv')
+    el_df = pd.read_csv('el/el_updated.csv')
+    mt_df = pd.read_csv('mt/mt_updated.csv')
+    pos_df = pd.read_csv('pos/pos_updated.csv')
+
+    selected_df = pd.read_csv(filename, index_col=0)
+    syn_df = selected_df[[col for col in selected_df.columns if col.startswith("S_")]]
+    inv_df = selected_df[[col for col in selected_df.columns if col.startswith("INV_")]]
+    phon_df = selected_df[[col for col in selected_df.columns if col.startswith("P_")]]
+
+    iso_to_glot_mapping = pd.read_csv('code_mapping.csv', index_col=0).to_dict()['glottocode']
+
+    for index, row in dep_df.iterrows():
+        target_lang = iso_to_glot_mapping[row['Target lang']]
+        transfer_lang = iso_to_glot_mapping[row['Transfer lang']]
+        lang_1_idx = selected_df.index.get_loc(target_lang)
+        lang_2_idx = selected_df.index.get_loc(transfer_lang)
+        dep_df.at[index, "SYNTACTIC"] = u._angular_distance(syn_df.iloc[lang_1_idx].to_numpy(), syn_df.iloc[lang_2_idx].to_numpy())
+        dep_df.at[index, "INVENTORY"] = u._angular_distance(inv_df.iloc[lang_1_idx].to_numpy(), inv_df.iloc[lang_2_idx].to_numpy())
+        dep_df.at[index, "PHONOLOGICAL"] = u._angular_distance(phon_df.iloc[lang_1_idx].to_numpy(), phon_df.iloc[lang_2_idx].to_numpy())
+        dep_df.at[index, "FEATURAL"] = u._angular_distance(selected_df.iloc[lang_1_idx].to_numpy(), selected_df.iloc[lang_2_idx].to_numpy())
+
+    dep_df.to_csv('dep/dep_selected.csv')
+
+    for index, row in el_df.iterrows():
+        target_lang = iso_to_glot_mapping[row['Target lang']]
+        transfer_lang = iso_to_glot_mapping[row['Transfer lang']]
+        lang_1_idx = selected_df.index.get_loc(target_lang)
+        lang_2_idx = selected_df.index.get_loc(transfer_lang)
+        el_df.at[index, "SYNTACTIC"] = u._angular_distance(syn_df.iloc[lang_1_idx].to_numpy(), syn_df.iloc[lang_2_idx].to_numpy())
+        el_df.at[index, "INVENTORY"] = u._angular_distance(inv_df.iloc[lang_1_idx].to_numpy(), inv_df.iloc[lang_2_idx].to_numpy())
+        el_df.at[index, "PHONOLOGICAL"] = u._angular_distance(phon_df.iloc[lang_1_idx].to_numpy(), phon_df.iloc[lang_2_idx].to_numpy())
+        el_df.at[index, "FEATURAL"] = u._angular_distance(selected_df.iloc[lang_1_idx].to_numpy(), selected_df.iloc[lang_2_idx].to_numpy())
+
+    el_df.to_csv('el/el_selected.csv')
+
+    for index, row in mt_df.iterrows():
+        target_lang = iso_to_glot_mapping[row['Target lang']]
+        source_lang = iso_to_glot_mapping[row['Source lang']]
+        transfer_lang = iso_to_glot_mapping[row['Transfer lang']]
+        lang_1_idx = selected_df.index.get_loc(target_lang)
+        lang_2_idx = selected_df.index.get_loc(transfer_lang)
+        lang_3_idx = selected_df.index.get_loc(source_lang)
+        mt_df.at[index, "SYNTACTIC"] = u._angular_distance(syn_df.iloc[lang_1_idx].to_numpy(), syn_df.iloc[lang_2_idx].to_numpy())
+        mt_df.at[index, "INVENTORY"] = u._angular_distance(inv_df.iloc[lang_1_idx].to_numpy(), inv_df.iloc[lang_2_idx].to_numpy())
+        mt_df.at[index, "PHONOLOGICAL"] = u._angular_distance(phon_df.iloc[lang_1_idx].to_numpy(), phon_df.iloc[lang_2_idx].to_numpy())
+        mt_df.at[index, "FEATURAL"] = u._angular_distance(selected_df.iloc[lang_1_idx].to_numpy(), selected_df.iloc[lang_2_idx].to_numpy())
+        mt_df.at[index, "SYNTACTIC_2"] = u._angular_distance(syn_df.iloc[lang_1_idx].to_numpy(), syn_df.iloc[lang_3_idx].to_numpy())
+        mt_df.at[index, "INVENTORY_2"] = u._angular_distance(inv_df.iloc[lang_1_idx].to_numpy(), inv_df.iloc[lang_3_idx].to_numpy())
+        mt_df.at[index, "PHONOLOGICAL_2"] = u._angular_distance(phon_df.iloc[lang_1_idx].to_numpy(), phon_df.iloc[lang_3_idx].to_numpy())
+        mt_df.at[index, "FEATURAL_2"] = u._angular_distance(selected_df.iloc[lang_1_idx].to_numpy(), selected_df.iloc[lang_3_idx].to_numpy())
+
+    mt_df.to_csv('mt/mt_selected.csv')
+
+    for index, row in pos_df.iterrows():
+        target_lang = iso_to_glot_mapping[row['Task lang']]
+        aux_lang = iso_to_glot_mapping[row['Aux lang']]
+        lang_1_idx = selected_df.index.get_loc(target_lang)
+        lang_2_idx = selected_df.index.get_loc(aux_lang)
+        pos_df.at[index, "SYNTACTIC"] = u._angular_distance(syn_df.iloc[lang_1_idx].to_numpy(), syn_df.iloc[lang_2_idx].to_numpy())
+        pos_df.at[index, "INVENTORY"] = u._angular_distance(inv_df.iloc[lang_1_idx].to_numpy(), inv_df.iloc[lang_2_idx].to_numpy())
+        pos_df.at[index, "PHONOLOGICAL"] = u._angular_distance(phon_df.iloc[lang_1_idx].to_numpy(), phon_df.iloc[lang_2_idx].to_numpy())
+        pos_df.at[index, "FEATURAL"] = u._angular_distance(selected_df.iloc[lang_1_idx].to_numpy(), selected_df.iloc[lang_2_idx].to_numpy())
+
+    pos_df.to_csv('pos/pos_selected.csv')
