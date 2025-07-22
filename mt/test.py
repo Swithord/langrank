d7d658e03840249e00695dba6969ad03a20a0be1
York Ng <yorkhayng@gmail.com>
2025-07-22 16:48:12 -0400

    move data files, remove old files

--- mt/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('mt_updated.csv')
-s = ['ara','est','aze','fas','msa']
-filtered_df = data[~data['Source lang'].isin(s) & ~data['Transfer lang'].isin(s)]
-filtered_df.to_csv('mt_removed_ambiguous_updated.csv',index=False)
